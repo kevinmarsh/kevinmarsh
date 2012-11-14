1541228ac8a431f9fca87156270cc23af87de3f9
Kevin Marsh <kevinmarsh3@gmail.com>
2012-11-14 23:02:27 +0000

    fixed up js, a bit cleaner, better ui. modified mail image so its vertically centered

--- js/script.js
+++ js/script.js
@@ -1,52 +1,51 @@
-function select(highlighted) {
-    $('.contentTitles div.selected').removeClass('selected');
-    $('.contentTitles .' + highlighted).addClass('selected');
-    $('.contentBody div').fadeOut().delay(400);
-    $('.contentBody .' + highlighted).fadeIn();
-}
 $(document).ready( function() {
-    $('.contentBody div').hide();
+    $('.contentBody div, footer').hide();
     $('.contentTitles div').click( function() {
         var $contentTitles = $(this).parent()
-        var section = $(this).attr("class");
-        if (!$contentTitles.hasClass('selected')) {
+        var clicked = $(this).attr("class");
+        if (!$contentTitles.hasClass('selected')) { // If this is the first time clicked
             $contentTitles.addClass('selected');
+            $('footer').slideDown();
             $(this).addClass('selected');
-            // $('.contentBody').css('border', '2px solid #222');
-            $('.contentBody .' + section).fadeIn();
-            $(document).keydown(function (e) {
-                var selected = $('.contentTitles .selected').attr("class").split(' ')[0];
-                var nextSection = {'contact': 'about', 'about': 'code', 'code': 'projects', 'projects': 'contact'};
-                var prevSection = {'about': 'contact', 'code': 'about', 'projects': 'code', 'contact': 'projects'};
-                var keyCode = e.keyCode || e.which;
-                var arrow = {left: 37, up: 38, right: 39, down: 40 };
-
-                if (keyCode == arrow.right || keyCode == arrow.up) {
-                    select(nextSection[selected]);
-                } else if (keyCode == arrow.left || keyCode == arrow.down) {
-                    select(prevSection[selected]);
-                }
-            });
+            $('.contentBody .' + clicked).fadeIn();
         }
-        else if (!$(this).hasClass('selected')){
-            select(section);
-        /*
-            $('.contentTitles div.selected').removeClass('selected');
-            $(this).addClass('selected');
-            $('.contentBody div').fadeOut().delay(400);
-            $('.contentBody .' + section).fadeIn();
-        */
+        else if (!$(this).hasClass('selected')){ // If not already selected
+            selectDiv(clicked, 0);
         }
     });
-    $('.contentTitles2 div').hover(
-        function () {
-            console.log($(this).children('p'));
-            $(this).children('p').addClass('showText');
-        },
-        function () {
-            console.log($(this).children('p'));
-            $(this).children('p').removeClass('showText');
+    $(document).keydown(function (e) {
+        var $contentTitles = $('.contentTitles')
+        if (!$contentTitles.hasClass('selected')) {
+            $contentTitles.addClass('selected');
+            $('footer').slideDown();
+            selectDiv('about', 0);
+        } else {
+            var selected = $('.contentTitles .selected').attr("class").split(' ')[0];
+            var keyCode = e.keyCode || e.which;
+            if (keyCode == 39) { // right
+                selectDiv(selected, 1);
+            } else if (keyCode == 37) { //left
+                selectDiv(selected, -1);
+            }
         }
-    );
-    // $('.contentTitles div img').attr('src', 'img/test.png');
+    });
+    $('.contentBody div span.link').click(function (){
+        var link = $(this).attr('class').split(' ')[1]
+        selectDiv(link, 0);
+    });
+    function selectDiv(clicked, dir) {
+        var next;
+        if (clicked == 'about' && dir == -1) {
+            next = 'contact';
+        } else if (clicked == 'contact' && dir == 1) {
+            next = 'about';
+        } else {
+            var divs = ['about', 'code', 'projects', 'contact'];
+            next = divs[$.inArray(clicked, divs) + dir]
+        };
+        $('.contentTitles div.selected').removeClass('selected');
+        $('.contentTitles .' + next).addClass('selected');
+        $('.contentBody div').fadeOut().delay(400);
+        $('.contentBody .' + next).fadeIn();
+    }
 })
