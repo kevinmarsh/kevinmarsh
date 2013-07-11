29b222ab90fee802480898d1711d431e723e75e8
Kevin Marsh <kevinmarsh3@gmail.com>
2013-07-11 18:09:20 +0100

    started to create a github news feed, very simple, need to test online

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
     $('.contentBody section').hide();
     $('nav li').click( function() {
-        var $nav = $('nav')
+        var $nav = $('nav');
         var clicked = $(this).attr("class");
         if (!$nav.hasClass('selected')) { // If this is the first time clicked
             $nav.addClass('selected');
@@ -14,7 +14,7 @@ $(document).ready( function() {
         }
     });
     $(document).keydown(function (e) {
-        var $nav = $('nav')
+        var $nav = $('nav');
         if (!$nav.hasClass('selected')) {
             $nav.addClass('selected');
             $('footer').slideDown();
@@ -30,7 +30,7 @@ $(document).ready( function() {
         }
     });
     $('.contentBody section span.link').click(function (){
-        var link = $(this).attr('class').split(' ')[1]
+        var link = $(this).attr('class').split(' ')[1];
         selectDiv(link, 0);
     });
     $('span.icon span').click(function () {
@@ -38,7 +38,7 @@ $(document).ready( function() {
         $('.contentBody section').fadeOut().delay(400);
         $('nav .contact').addClass('selected');
         $('.contentBody .iconAttributes').fadeIn();
-    })
+    });
     function selectDiv(clicked, dir) {
         var next;
         if (clicked == 'about' && dir == -1) {
@@ -47,11 +47,75 @@ $(document).ready( function() {
             next = 'about';
         } else {
             var divs = ['about', 'code', 'projects', 'contact'];
-            next = divs[$.inArray(clicked, divs) + dir]
-        };
+            next = divs[$.inArray(clicked, divs) + dir];
+        }
         $('nav li.selected').removeClass('selected');
         $('nav .' + next).addClass('selected');
         $('.contentBody section').fadeOut().delay(400);
         $('.contentBody .' + next).fadeIn();
     }
-})
+    // TODO: test with live data once I've got internet access
+    // loadGitFeed();
+    $('#gitFeed').hide();
+});
+
+function loadGitFeed() {
+    $.ajax({
+        // dataType: "jsonp",  // TODO: switch back to JSONP after it's cross browser
+        dataType: "json",
+        // url: 'https://github.com/kevinmarsh.json',
+        url: 'kevinmarsh-github.json',  // TODO: remove test data
+        success: function(data){
+            printOutput(data);
+        },
+        error: function(e) {
+            console.log('error');
+        }
+    });
+}
+
+function printOutput(data){
+    var count = 0;
+    var gitActionVerb = {
+        PushEvent: 'Pushed',
+        CreateEvent: 'Created',
+        ForkEvent: 'Forked',
+        IssueCommentEvent: 'Commented on',
+        GollumEvent: '???',
+        MemberEvent: '???'  // TODO: What other events?
+    };
+    var $ul = $('#gitFeed ul');
+
+    // TODO: just replace this with a basic counter
+    for (var action in data) {
+        var gitAction = {
+            type: data[action]['type'],
+            repoName: '',
+            dateTime: new Date(data[action]['created_at']),
+            commitMsg: '',
+            url: $.trim(data[action]['url'])
+        };
+        var $el = $('<li></li>');
+        var msg = '';
+
+        if (count++ == 10) {
+            break;
+        }
+        try {
+            gitAction.commitMsg = data[action]['payload']['shas'][0][2];
+        } catch(e) {
+            // Do nothing
+        }
+        try {
+            gitAction.repoName = data[action]['repository']['name'];
+        } catch(e) {
+            gitAction.repoName = data[action]['url'].split('/')[4];
+        } finally {
+            msg = [gitActionVerb[gitAction.type], '<strong>', gitAction.repoName, '</strong> at <a href="', gitAction.url, '" title="' + gitAction.dateTime + '">', gitAction.dateTime.toLocaleDateString(), gitAction.dateTime.toLocaleTimeString(), '</a>'];
+            if (gitAction.commitMsg) {
+                msg.push('<br> "' + gitAction.commitMsg + '"');
+            }
+            $el.html(msg.join(' ')).appendTo($ul);
+        }
+    }
+}
