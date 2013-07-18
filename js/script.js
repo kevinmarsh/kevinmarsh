ee088c64137bc501dd9530721593da7ad2581ab9
Kevin Marsh <kevinmarsh3@gmail.com>
2013-07-18 10:44:36 +0100

    Improved support for browsers that don't support window.history, noticed bug in #! though (ln 21)

--- js/script.js
+++ js/script.js
@@ -2,43 +2,37 @@
 // the same push event.
 var gitEventCount = 0;
 
-$(document).ready( function() {
+$(document).ready(function() {
     // Allows for non-JS browsers to show all content
     $('.contentBody section').hide();
 
     // History State Change
     var path = window.location.pathname.split('/').pop();
+    path = path || window.location.hash.replace('#!', '');
     if (path !== '') {
         // Page is being loaded directly from URL with section already selected
         document.title = 'Kevin Marsh - Web Developer - ' + path.charAt(0).toUpperCase() + path.slice(1);
         $('nav, li.' + path).addClass('selected');
         $('.contentBody .' + path).fadeIn();
     }
-    window.onpopstate = function(event) {
-        var path = window.location.pathname.split('/').pop();
-        if (path) {
-            document.title = 'Kevin Marsh - Web Developer - ' + path.charAt(0).toUpperCase() + path.slice(1);
-            $('nav li.selected').removeClass('selected');
-            $('nav .' + path).addClass('selected');
-            $('.contentBody section').fadeOut().delay(400);
-            $('.contentBody .' + path).fadeIn();
-            if (!isInitialized()) {
-                $('nav').addClass('selected');
-            }
-        } else {
-            resetPage();
-        }
-    };
+    if (typeof window.history.pushState === 'function') {
+        window.onpopstate = onUrlChange;
+    } else {
+        // TODO: This hash change is triggering loading the content twice,
+        //       first when user clicks on the icon and then when the hash changes
+        //       Maybe need to add a global hist detector
+        window.onhashchange = onUrlChange;
+    }
 
     // Click Navigation
-    $('nav li').click( function() {
+    $('nav li').click(function() {
         var clicked = $(this).attr("class");
         if (!isInitialized()) {
             // If this is the first time clicked
             updateHistory(clicked);
             $(this, 'nav').addClass('selected');
             $('.contentBody .' + clicked).fadeIn();
-        } else if (!$(this).hasClass('selected')){
+        } else if (!$(this).hasClass('selected')) {
             // If not already selected
             selectDiv(clicked, 0);
         }
@@ -62,7 +56,7 @@ $(document).ready( function() {
 
     // Spans that act as internal links
     // TODO: just change these to anchors once .htaccess is updated
-    $('.contentBody section span.link').click(function (){
+    $('.contentBody section span.link').click(function() {
         var link = $(this).attr('class').split(' ')[1];
         selectDiv(link, 0);
     });
@@ -84,26 +78,10 @@ $(document).ready( function() {
         window.open('https://github.com/kevinmarsh?tab=activity');
     });
 
-    function selectDiv(clicked, dir) {
-        var next;
-        if (clicked === 'about' && dir === -1) {
-            next = 'contact';
-        } else if (clicked === 'contact' && dir === 1) {
-            next = 'about';
-        } else {
-            var divs = ['about', 'code', 'projects', 'contact'];
-            next = divs[$.inArray(clicked, divs) + dir];
-        }
-        updateHistory(next);
-        $('nav li.selected').removeClass('selected');
-        $('nav .' + next).addClass('selected');
-        $('.contentBody section').fadeOut().delay(400);
-        $('.contentBody .' + next).fadeIn();
-    }
     loadGitFeed();
-    $('#gitFeed li[url]').live('click', function(){
+    $('#gitFeed li[url]').live('click', function() {
         // Changes the url from API to public facing
-        window.open($(this).attr('url').replace('api.github.com/repos', 'github.com'). replace('commits', 'commit'));
+        window.open($(this).attr('url').replace('api.github.com/repos', 'github.com').replace('commits', 'commit'));
     });
 });
 
@@ -111,37 +89,69 @@ function isInitialized() {
     // Returns true if a content section is loaded
     return $('nav').hasClass('selected');
 }
-function resetPage () {
+function resetPage() {
     $('nav, nav li').removeClass('selected');
     $('.contentBody section').fadeOut().delay(400);
     document.title = 'Kevin Marsh - Web Developer';
 }
 
+function onUrlChange() {
+    var path = window.location.pathname.split('/').pop();
+    path = path || window.location.hash.replace('#!', '');
+    if (path) {
+        document.title = 'Kevin Marsh - Web Developer - ' + path.charAt(0).toUpperCase() + path.slice(1);
+        $('nav li.selected').removeClass('selected');
+        $('nav .' + path).addClass('selected');
+        $('.contentBody section').fadeOut().delay(400);
+        $('.contentBody .' + path).fadeIn();
+        if (!isInitialized()) {
+            $('nav').addClass('selected');
+        }
+    } else {
+        resetPage();
+    }
+}
+
 function updateHistory(path) {
     // Updates the page title, adds the page to history and changes url
     document.title = 'Kevin Marsh - Web Developer - ' + path.charAt(0).toUpperCase() + path.slice(1);
-    if (typeof(window.history.pushState) == 'function') {
+    if (typeof window.history.pushState === 'function') {
         window.history.pushState(null, path, path);
     } else {
         window.location.hash = '#!' + path;
     }
 }
 
+function selectDiv(clicked, dir) {
+    var next;
+    if (clicked === 'about' && dir === -1) {
+        next = 'contact';
+    } else if (clicked === 'contact' && dir === 1) {
+        next = 'about';
+    } else {
+        var divs = ['about', 'code', 'projects', 'contact'];
+        next = divs[$.inArray(clicked, divs) + dir];
+    }
+    updateHistory(next);
+    $('nav li.selected').removeClass('selected');
+    $('nav .' + next).addClass('selected');
+    $('.contentBody section').fadeOut().delay(400);
+    $('.contentBody .' + next).fadeIn();
+}
 function loadGitFeed() {
     $.ajax({
         dataType: 'jsonp',
         url: 'https://api.github.com/users/kevinmarsh/events',
-        success: function(data){
+        success: function(data) {
             printGitEvents(data.data);
         },
-        error: function(e) {
-            console.log('Error: Something went wrong.');
+        error: function() {
             $('#gitFeed').hide();
         }
     });
 }
 
-function printGitEvents(data){
+function printGitEvents(data) {
     // This takes the JSON data from the AJAX request,
     // formats it and then appends it to the list.
     var $ul = $('#gitFeed ul');
@@ -155,7 +165,7 @@ function printGitEvents(data){
             $ul = createPushEvents(data[gitEventCount], $ul);
         } else {
             var $li = $('<li></li>').addClass(gitType).attr('gitid', data[i].id);
-            var dateTime = new Date(data[gitEventCount]['created_at']);
+            var dateTime = new Date(data[gitEventCount].created_at);
             if (gitType === 'PullRequestEvent') {
                 $li.attr('url', data[gitEventCount].payload.pull_request.html_url);
             }
